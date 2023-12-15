957c4020dde94863e95a6625063b2b98695145e6
JonathanStillman <68572893+JonathanStillman@users.noreply.github.com>
2023-12-15 09:06:22 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,5 @@
+# Declare a variable named "host_os"
+variable "host_os" {
+  type    = string          # Specify the variable type as a string
+  default = "linux"         # Set a default value for the variable
+}
