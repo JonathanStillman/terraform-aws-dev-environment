9794ecd6763d80b8efe74fe9c1b11c31d91f9816
JonathanStillman <68572893+JonathanStillman@users.noreply.github.com>
2023-12-15 09:05:37 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,4 @@
+# Declare the required providers and versions
 terraform {
   required_providers {
     aws = {
@@ -7,9 +8,9 @@ terraform {
   }
 }
 
-# Shared Configuration and Credentials Files
+# Configure the AWS provider
 provider "aws" {
-  region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/creds"]
-  profile                  = "vscode"
+  region                   = "us-east-1"          # Set the AWS region
+  shared_credentials_files = ["~/.aws/creds"]      # Specify the shared credentials file path
+  profile                  = "vscode"              # Use the specified AWS profile for authentication
 }
